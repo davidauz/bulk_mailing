8f173d00d4cf972add6c217306467fdaab20de67
Baby Balrog <davide.saccon@gmail.com>
2023-03-11 08:32:15 +0800

    AJX done right ... ?

--- interface/src/main/java/com/davidauz/bulk_mailing/controller/projectsController.java
+++ interface/src/main/java/com/davidauz/bulk_mailing/controller/projectsController.java
@@ -1,22 +1,27 @@
 package com.davidauz.bulk_mailing.controller;
 
-import com.davidauz.bulk_mailing.entity.Company;
 import com.davidauz.bulk_mailing.entity.Project;
 import com.davidauz.bulk_mailing.repository.CompanyRepository;
 import com.davidauz.bulk_mailing.repository.GroupRepository;
 import com.davidauz.bulk_mailing.repository.PersonRepository;
 import com.davidauz.bulk_mailing.repository.ProjectsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class projectsController {
@@ -76,5 +81,36 @@ public class projectsController {
         return "forms/project_form";
     }
 
+
+
+    @PostMapping("/project/ajx_dispatcher")
+    public ResponseEntity<String> handleAjxRequest
+    (   @RequestBody Map<String, Object> requestData
+    ) {
+        String str_verb= (String) requestData.get("verb");
+        if (str_verb == null)
+            return ResponseEntity.badRequest().body("VERB parameter is missing");
+        switch(str_verb){
+            case "add_groups":
+                return add_groups((ArrayList<String>) requestData.get("groups"));
+        }
+        return ResponseEntity.ok("success");
+    }
+
+    private ResponseEntity<String> add_groups(ArrayList<String> groups) {
+        String[] ret_data=new String[] {"a", ":", "c"};
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = null;
+        try {
+            jsonString = objectMapper.writeValueAsString(ret_data);
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<String>(jsonString, responseHeaders, HttpStatus.OK);
+    }
+
+//Controllare nel POM.xml se servono veramente tutte quelle dependencysu JSON
 
 }
