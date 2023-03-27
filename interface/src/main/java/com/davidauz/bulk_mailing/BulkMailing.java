7a252b8540d011ec4c64e293c36a09b53f2b784d
Baby Balrog <davide.saccon@gmail.com>
2023-03-27 16:55:51 +0800

    Run with java -jar ...

--- interface/src/main/java/com/davidauz/bulk_mailing/BulkMailing.java
+++ interface/src/main/java/com/davidauz/bulk_mailing/BulkMailing.java
@@ -2,12 +2,19 @@ package com.davidauz.bulk_mailing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BulkMailing {
+public class BulkMailing // extends SpringBootServletInitializer
+ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BulkMailing.class, args);
 	}
 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(BulkMailing.class);
+//	}
 }
