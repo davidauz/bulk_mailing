51c0b6e445f01dcb5d348e02d7d804edb04b10f0
Baby Balrog <davide.saccon@gmail.com>
2023-03-29 10:10:49 +0800

    Adding message_id to messages

--- mailer_daemon/src/main/java/com/davidauz/bulk_mailing/mailer_daemon/sendEmailService.java
+++ mailer_daemon/src/main/java/com/davidauz/bulk_mailing/mailer_daemon/sendEmailService.java
@@ -10,15 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class sendEmailService {
@@ -37,6 +39,8 @@ public class sendEmailService {
 
     @Value("${mail.test:false}")
     private Boolean mailTest = false;
+
+    private DateFormat m_dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSS");
 
     void sendOneEmail(long mail_id) throws Exception {
         Optional<blk_MailMessage> o_blkm = Optional.ofNullable(mailMessageRepo.findById(mail_id).orElseThrow(() -> new Exception("mail ID '" + mail_id + "' not found")));
@@ -60,6 +64,13 @@ public class sendEmailService {
             helper.setTo(blkm.getRecipient());
             helper.setSubject(blkm.getSubject());
             helper.setText(text_body, true);
+
+//            DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSS");
+            Date date = new Date();
+            String messageId = "bm_"+ m_dateFormat.format(date)+"_"+ UUID.randomUUID();
+            mimeMessage.setHeader("Message-ID", "<" + messageId + ">");
+            blkm.setMessageId(messageId);
+
             JMailSender.send(mimeMessage);
             now_timestamp = new Timestamp(Calendar.getInstance().getTime().getTime());
             blkm.setTimeSent(now_timestamp);
